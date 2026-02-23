3c81e3cdbc82dcf3970983b62557c2860596fe0f
kentivan63-art <kentivan63@gmail.com>
2026-02-24 00:47:24 +0800

    Additional simple codes in php for connecting to the sql server using the xampp

---
+++ frontEnd/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Website</title>
+</head>
+<body>
+    <h1>Welcome to My Site</h1>
+
+    <?php
+    // You can write PHP right here in the middle of your HTML!
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "simple_web";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        echo "<p style='color:red;'>Database offline.</p>";
+    } else {
+        echo "<p style='color:green;'>Database connected successfully!</p>";
+    }
+    ?>
+
+    <form action="insert.php" method="POST">
+        <input type="text" name="user_name" placeholder="Enter Name">
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
